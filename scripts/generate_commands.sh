84d13effdc2bf6993fbdc0b11d305196ba3c0a59
natecrow <nate.crow@runbox.com>
2021-06-26 17:03:52 -0700

    Ignore demo files. Add python gen script cmds to file to help re-gen themes quicker.

---
+++ scripts/generate_commands.sh
@@ -0,0 +1,21 @@
+# light themes
+python scripts/generate-theme-light.py blue-270-30-light 270 30
+python scripts/generate-theme-light.py gold-90-210-light 90 210
+python scripts/generate-theme-light.py green-120-240-light 120 240
+python scripts/generate-theme-light.py green-150-270-light 150 270
+python scripts/generate-theme-light.py orange-60-300-light 60 300
+python scripts/generate-theme-light.py purple-315-75-light 315 75
+python scripts/generate-theme-light.py red-30-270-light 30 270
+python scripts/generate-theme-light.py teal-210-90-light 210 90
+python scripts/generate-theme-light.py violet-300-60-light 300 60
+
+# dark themes
+# python scripts/generate-theme-dark.py blue-270-30 270 30
+# python scripts/generate-theme-dark.py gold-90-210 90 210
+# python scripts/generate-theme-dark.py green-120-240 120 240
+# python scripts/generate-theme-dark.py green-150-270 150 270
+# python scripts/generate-theme-dark.py orange-60-300 60 300
+# python scripts/generate-theme-dark.py purple-315-75 315 75
+# python scripts/generate-theme-dark.py red-30-270 30 270
+# python scripts/generate-theme-dark.py teal-210-90 210 90
+# python scripts/generate-theme-dark.py violet-300-60 300 60
