93c13866a25bed81784f413d4a1c682a1732d69e
natecrow <nate.crow@runbox.com>
2021-09-10 16:35:55 -0700

    Redo light themes similarly to dark themes

--- scripts/generate-all-themes.sh
+++ scripts/generate-all-themes.sh
@@ -10,15 +10,13 @@
 
 # Light themes
 #==============
-# python generate-theme-light.py blue-270-30 270 30
-# python generate-theme-light.py gold-90-210 90 210
-# python generate-theme-light.py green-120-240 120 240
-# python generate-theme-light.py green-150-270 150 270
-# python generate-theme-light.py orange-60-300 60 300
-# python generate-theme-light.py purple-300-60 300 60
-# python generate-theme-light.py purple-320-80 320 80
-# python generate-theme-light.py red-30-270 30 270
-# python generate-theme-light.py teal-210-90 210 90
+python generate-theme-light.py blue 270 30
+python generate-theme-light.py cyan 225 105
+python generate-theme-light.py gold 90 210
+python generate-theme-light.py green 135 255
+python generate-theme-light.py orange 45 285
+python generate-theme-light.py purple 315 75
+python generate-theme-light.py teal 180 300
 
 # Remove quotes around "true" in generated themes so it becomes a boolean instead of a string
 sed -i 's/\"true\"/true/g' *color-theme.json
